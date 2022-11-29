6b73a301eb66ae4b60fea5f26595e38383e1fd9c
ddarshi97 <118235906+ddarshi97@users.noreply.github.com>
2022-11-29 18:13:14 +0530

    Update tominst.sh

--- tominst.sh
+++ tominst.sh
@@ -1,20 +1,9 @@
-#!/bin/bash
-sudo apt update -y
-sudo apt install default-jdk -y
-sudo wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.0.27/bin/apache-tomcat-10.0.27.tar.gz
-sudo tar -xvzf apache-tomcat-10.0.27.tar.gz
-sudo rm -rf apache-tomcat-10.0.27.tar.gz
-sudo mv apache-tomcat-10.0.27 /opt/tomcat
-sudo sh /opt/tomcat/bin/startup.sh
-sudo rm -rf conf-and-webapps-file
-sudo git clone https://github.com/syedwaliuddin/conf-and-webapps-file.git
-sudo rm -rf /opt/tomcat/conf/tomcat-users.xml
-sudo cp conf-and-webapps-file/tomcat-users.xml /opt/tomcat/conf/
-sudo sh /opt/tomcat/bin/startup.sh
-sudo rm -rf /opt/tomcat/webapps/manager/META-INF/context.xml
-sudo cp conf-and-webapps-file/context.xml /opt/tomcat/webapps/manager/META-INF/
-sudp rm -rf /opt/tomcat/webapps/host-manager/META-INF/context.xml
-sudo cp conf-and-webapps-file/contexthm.xml /opt/tomcat/webapps/host-manager/META-INF/
-sudo sh /opt/tomcat/bin/startup.sh
-sudo rm -rf conf-and-webapps-file
-sudo rm -rf apache-tomcat-10.0.27
+#!/bin/bash/
+sudo su -
+apt update
+apt install default-jdk
+cd /opt
+wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.0.27/bin/apache-tomcat-10.0.27.tar.gz
+tar -xvzf apache-tomcat-10.0.27.tar.gz
+sh apache-tomcat-10.0.27/bin/startup.sh
+echo "change the traffic to all traffic and all IPV4"
