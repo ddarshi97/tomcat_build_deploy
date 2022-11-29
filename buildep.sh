fc0eb42aef463d419225bf1bc780a6e7cc99426e
ddarshi97 <118235906+ddarshi97@users.noreply.github.com>
2022-11-29 17:28:56 +0530

    Update buildep.sh

--- buildep.sh
+++ buildep.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf hello-world-war
-git clone https://github.com/kevalohith/hello-world-war.git
+git clone https://github.com/ddarshi97/hello-world-war.git
 cd hello-world-war
 mvn package
 sudo cp -R taget/hello-world-war-1.0.0.war /opt/apcahe-tomcat-10.0.27/webapps/
